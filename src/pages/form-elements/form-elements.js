1c3515ea68cc1404fdbfa5ee4e507aae9ba9d645
Marat Gumerov <gumerovm74@gmail.com>
2020-06-20 10:41:08 +0300

    part-1-5-3-form: loyout components

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -1,4 +1,5 @@
 import './form-elements'
 
 // Импорт компонентов
-import { dropdown } from '../../components/dropdown/dropdown.js'
+import {dropdown} from '../../components/dropdown/dropdown.js'
+import {MaskedTextField} from '../../components/masked-text-field/masked-text-field';
